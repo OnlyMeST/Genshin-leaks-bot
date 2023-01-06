19cc445fc5cd6547fce7d79cbfbdb3d08b784947
OnlyMeST <104155192+OnlyMeST@users.noreply.github.com>
2023-01-06 19:52:40 +0100

    Update Telegram_Bot.py

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -3,97 +3,63 @@
 #Libraries and shit
 from __future__ import unicode_literals
 import telegram
+import requests
 import praw
-import logging
 import html
-import sys
 import os
-import json
 from time import sleep
 from datetime import datetime
+
+print("We got here")
 
 #Important bs
 
 credentials = {}
 
-credentials["token"] = os.getenv('TOKEN')
-credentials["subreddit"] = os.getenv('SUB')
-credentials["channel"] = os.getenv('CHANNEL')
+credentials["token"] ="5944534549:AAGI8YaTj3_P5YgMy-FMalVbfGUS1VNHneg"
+credentials["subreddit"] = "https://reddit.com/r/Genshin_impact_leaks"
+credentials["channel"] = "https://t.me/genshinimpact_leakschannel"
 token = credentials["token"]
 channel = credentials["channel"]
 sub = "Genshin_Impact_Leaks"
 start_time = datetime.utcnow().timestamp()
 
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-log.addHandler(ch)
-
-if credentials["token"] == "":
-    raise RuntimeError('Bot token not found')
-if credentials["subreddit"] == "":
-    raise RuntimeError('Subreddit name not found ')
-if credentials["channel"] == "":
-    raise RuntimeError('Telegram Channel name not found ')
-
-def prev_submissions():
-    try:
-        with open('prev_submissions.id', 'r') as f:
-            return f.read().strip()
-    except:
-        return None
-
-def write_submissions(sub_id):
-    try:
-        with open('prev_submissions.id', 'w') as f:
-            f.write(sub_id)
-    except:
-        log.expection("Error writing sub ID!")
-
-post = False
-last_sub_id = prev_submissions()
-
-if not last_sub_id:
-    log.info("Latest submission not found, starting all submissions!")
-    post = True
-else:
-    log.info("Last posted submission is {}".format(last_sub_id))
+print("and now here")
 
 r = praw.Reddit(user_agent="Genshin Impact Leaks",
-                client_id=os.getenv('CLIENT_ID'),
-                client_secret=os.getenv('CLIENT_SECRET'),
-                username=os.getenv('RUSERNAME'),
-                password=os.getenv('RPASS'))
+                client_id="kLXmbgYfFp_OcaKKmqBGTg",
+                client_secret="4c33RFT8i2Iub6LEU35w2pV49QGA0g",
+                username="STDeveloper",
+                password="ringof666timeup")
 r.read_only = True
 subreddit = r.subreddit(sub)
 
 bot = telegram.Bot(token=token)
 
+print("gonna end soon")
+
+
+def post_to_telegram():
+  try:
+    for submission in subreddit.hot():
+      image = html.escape(submission.url or '')
+      title = html.escape(submission.title or '')
+      link = "https://redd.it/{id}".format(id=submission.id)
+      template = "{title}\n\n Channel: @Genshinimpact_leakschannel"
+      message = template.format(title=title, link=link)
+      r = requests.get('https://api.telegram.org/bot{}/sendPhoto'.format(
+        token),
+                       json={
+                         "chat_id": channel,
+                         "photo": image,
+                         "caption": message
+                       })
+      if r.ok:
+        bot.sendPhoto(chat_id=channel, photo=image, caption=message)
+        print("Posted. Check your channel")
+        #sleep(600)
+  except requests.exceptions.RequestException as e:  # This is the correct syntax
+    raise SystemExit(e)
+
 while True:
-    try:
-        for submission in subreddit.hot():
-            try:
-                link = "https://redd.it/{id}".format(id=submission.id)
-                if not post and submission.created_utc < start_time:
-                    log.info("Skipping {} --- latest submission not found!".format(submission.id))
-                    if submission.id == last_sub_id:
-                        post = True
-                    continue
-                image = html.escape(submission.url or '')
-                title = html.escape(submission.title or '')
-                user = html.escape(submission.author.name or '')
-
-                template = "{title}\n{link}\nby {user}"
-                message = template.format(title=title, link=link, user=user)
-
-                log.info("Posting {}".format(link))
-                bot.sendPhoto(chat_id=channel, photo=submission.url, caption=message)
-                write_submissions(submission.id)
-                sleep(600)
-            except Exception as e:
-                log.exception("Error parsing {}".format(link))
-    except Exception as e:
-        log.exception("Error fetching new submissions, restarting in 10 secs")
-        sleep(10)
-
+  post_to_telegram()
